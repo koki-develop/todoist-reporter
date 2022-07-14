6a83436b438b693ce2fd0b5eecfe31d00963f412
koki-develop <kou.pg.0131@gmail.com>
2022-07-14 21:30:50 +0900

    Due を削除

--- pkg/todoist/item.go
+++ pkg/todoist/item.go
@@ -3,19 +3,14 @@ package todoist
 import "github.com/koki-develop/todoist-reporter/pkg/util"
 
 type Item struct {
-	ID        int      `json:"id"`
-	ParentID  *int     `json:"parent_id"`
-	ProjectID int      `json:"project_id"`
-	SectionID int      `json:"section_id"`
-	Content   string   `json:"content"`
-	LabelIDs  []int    `json:"labels"`
-	Due       *ItemDue `json:"due"`
+	ID        int    `json:"id"`
+	ParentID  *int   `json:"parent_id"`
+	ProjectID int    `json:"project_id"`
+	SectionID int    `json:"section_id"`
+	Content   string `json:"content"`
+	LabelIDs  []int  `json:"labels"`
 
 	Children Items
-}
-
-type ItemDue struct {
-	Date string `json:"date"`
 }
 
 type Items []*Item
